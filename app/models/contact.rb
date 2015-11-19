97b20c0779860701d1d400d3fed1b36e11deea5d
Peter Jang <peter.x.jang@gmail.com>
2015-11-18 20:10:57 -0600

    added model methods

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,13 @@
 class Contact < ActiveRecord::Base
+  def friendly_updated_at
+    updated_at.strftime("%B %e, %Y")
+  end
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
+
+  def japan_phone_number
+    "+81 #{phone}"
+  end
 end
